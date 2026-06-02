d1f756f92621f5624ec8bef3d180598535162c3b
KhanSharif786 <001.ksharif@gmail.com>
2026-06-02 15:03:47 +0530

    Allow proxy headers

--- server.py
+++ server.py
@@ -810,5 +810,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
+        port=int(os.environ.get("PORT", 10000)),
+        forwarded_allow_ips="*",
+        proxy_headers=True
     )
